44e8042f047c4efac29ea9a6a6e40feccfb5bc68
Antoj2000 <g00385306@atu.ie>
2025-11-01 23:10:53 +0000

    added db and integrated lab4 minimally

--- app/main.py
+++ app/main.py
@@ -1,36 +1,67 @@
 # app/main.py
 
-from fastapi import FastAPI, HTTPException, status
-from .schemas import User
+from fastapi import FastAPI, HTTPException, status, Depends, Response
+from .database import SessionLocal, engine
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session, selectinload
+from .schemas import(
+    UserCreate, UserRead
+)
+from .models import AccountsDB, Base
 
 app = FastAPI()
-users: list[User] = []
+
+# Uncomment this line to reset DB
+# Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def commit_or_rollback(db: Session, error_msg: str):
+    try:
+        db.commit()
+    except IntegrityError:
+        db.rollback
+
+@app.get("/health")
+def health():
+    return {"status" : "ok"}
+
+# --- Accounts
 
 #Get all users
-@app.get("/api/users")
-def get_users():
-    return users
+@app.get("/api/accounts", response_model=list[UserRead])
+def list_accounts(db: Session = Depends(get_db)):
+    stmt = select(AccountsDB).order_by(AccountsDB.id)
+    return db.execute(stmt).scalars().all()
 
 #Get user by id
-@app.get("/api/users/{user_id}")
-def get_user(user_id: int):
+#@app.get("/api/users/{user_id}")
+#def get_user(user_id: int):
     for u in users:
         if u.user_id == user_id:
             return u
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 #Create user
-@app.post("/api/users", status_code=status.HTTP_201_CREATED)
-def add_user(user: User):
+@app.post("/api/accounts", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+def create_account(account: UserCreate, db: Session = Depends(get_db)):
     #Check if user already exists 
-    if any(u.user_id == user.user_id for u in users):
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id already exists")
-    users.append(user) #append means to add to list
-    return user
+    db_account = AccountsDB(**account.model_dump())
+    db.add(db_account)
+    commit_or_rollback(db, "Account already exists")
+    db.refresh(db_account)
+    return db_account
     
 #Update existing user
-@app.put("/api/users/update/{user_id}")
-def edit_user(user_id: int, edited_user: User): # user id from URL and user object from payload
+#@app.put("/api/users/update/{user_id}")
+#def edit_user(user_id: int, edited_user: User): # user id from URL and user object from payload
     for i, u in enumerate(users): # checks list of users 
         if u.user_id == user_id: # match ids
             users[i] = edited_user # replace the old user with the new one
@@ -39,8 +70,8 @@ def edit_user(user_id: int, edited_user: User): # user id from URL and user obje
 
 
 #Delete existing user
-@app.delete("/api/users/delete/{user_id}", status_code=status.HTTP_204_NO_CONTENT) #if endpoint succeeds return status code 
-def delete_user(user_id: int): # user ID from URL
+#@app.delete("/api/users/delete/{user_id}", status_code=status.HTTP_204_NO_CONTENT) #if endpoint succeeds return status code 
+#def delete_user(user_id: int): # user ID from URL
     for u in users: # checks list 
         if u.user_id == user_id: # match ids 
             users.remove(u) # remove user
@@ -48,9 +79,4 @@ def delete_user(user_id: int): # user ID from URL
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found") #404 if user doesnt exist 
                 
 
-#Health endpoint
-@app.get("/api/health", status_code=status.HTTP_200_OK)
-def health():
-    return "OK"
-    
         
--- app/schemas.py
+++ app/schemas.py
@@ -1,24 +1,37 @@
 # app/schemas.py
-from pydantic import BaseModel, EmailStr, constr, conint, field_validator
+from pydantic import BaseModel, EmailStr, constr, conint, field_validator, StringConstraints, Field
+from typing import Annotated, Optional, List
 
 
-class User(BaseModel):
-    user_id: int
-    account_no: constr(pattern=r'^A\d{5}$') # used pattern instead of regex as python v2 no longer uses regex
-    name: constr(min_length=2, max_length=50)
+# ---------- Reusable Type aliases -----------
+
+AccountStr = Annotated[str, constr(pattern=r'^A\d{5}$')]
+NameStr = Annotated[str, StringConstraints(min_length=2, max_length=50)]
+PasswordStr = Annotated[str, StringConstraints(min_length=8, max_length=20)]
+
+class UserCreate(BaseModel):
+
+    account_no: AccountStr 
+    name: NameStr
     email: EmailStr
-    password: constr(min_length=8) 
+    password: PasswordStr 
 
     @field_validator('password')
-    def password_must_have_upper_and_digit(cls, passWord):
-        if not any(c.islower() for c in passWord):
+    def password_must_have_upper_and_digit(cls, password):
+        if not any(c.islower() for c in password):
             raise ValueError('Password must contain at least one lowercase letter')
-        if not any(c.isupper() for c in passWord):
+        if not any(c.isupper() for c in password):
             raise ValueError('Password must contain at least one uppercase letter')
-        if not any(c.isdigit() for c in passWord):
+        if not any(c.isdigit() for c in password):
             raise ValueError('Password must contain at least one digit')
-        if len(passWord) < 8:
-            raise ValueError('Password must be at least 8 characters long')
-        if len(passWord) > 20:
-            raise ValueError('Password must be at most 20 characters long')
-        return passWord
+        return password
+
+
+class UserRead(BaseModel):
+    id : int
+    account_no: AccountStr
+    name: NameStr
+    email: EmailStr
+    password: PasswordStr
+
+
