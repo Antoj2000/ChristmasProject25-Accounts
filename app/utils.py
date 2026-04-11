addf41455852018735c64faa738e9084909ff807
Antoj2000 <g00385306@atu.ie>
2026-04-11 21:59:49 +0100

    app changes

--- app/utils.py
+++ app/utils.py
@@ -14,3 +14,14 @@ def assign_number_range(db: Session):
 
     end = start + RANGE_SIZE -1
     return start, end
+
+def generate_next_account_no(db: Session) -> str:
+    last_account = db.query(AccountsDB).order_by(AccountsDB.id.desc()).first()
+
+    if last_account and last_account.account_no:
+        last_number = int(last_account.account_no[1:])
+        next_number = last_number + 1
+    else:
+        next_number = 1
+
+    return f"A{next_number:05d}"
